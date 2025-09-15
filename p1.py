b27d05f04bbc699ffae08e62bfd3b7d4cad6220c
emmaqqu <emma28q@gmail.com>
2025-09-15 10:51:28 -0400

    created a python file to be creepy.

---
+++ p1.py
@@ -0,0 +1,8 @@
+# My first Python program
+# Created on 09/15/2025
+# Purpose: To learn Python
+# Editor: Emma
+
+name = input("What is your name? ")
+age = input("What is your age? ")
+print(f"Hi {name}! I heard you're now {age}, it's been so long.")
